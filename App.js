90338a62a55e8164cc977b7e4395429f46aa7fbd
meferbas <40mefe20@gmail.com>
2024-11-29 17:07:32 +0300

    Firebase included the project

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { initializeApp } from "firebase/app";
+import { View, Text } from "react-native";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCY4YWyZfitmebC_p61n72bp4rToODcGYI",
+  authDomain: "e-laboratory-mobile-project.firebaseapp.com",
+  projectId: "e-laboratory-mobile-project",
+  storageBucket: "e-laboratory-mobile-project.appspot.com",
+  messagingSenderId: "632177386698",
+  appId: "1:632177386698:web:14e06884b766e52c9cc033",
+  measurementId: "G-RNXNZR2GVF",
+};
+
+// Firebase'i başlat
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20, color: "green" }}>Firebase is working!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
